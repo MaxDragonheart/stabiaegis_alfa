0341662ecc493b92e5f49209e9c48c3c095d5985
MaxDragonheart <massimilianomoraca@gmail.com>
2022-11-17 21:52:42 +0100

    dev project

--- assets/js/layers_sit_cmn.js
+++ assets/js/layers_sit_cmn.js
@@ -1,22 +1,6 @@
 // List of layers from Città Metropolitana di Napoli
 var wmsSource = 'http://sit.cittametropolitana.na.it/geoserver/sit/ows?service=wms'
 
-/*var ambitiSistemaPorti = new ol.layer.Tile({
-    source: new ol.source.TileWMS({
-    url: wmsSource,
-    params: {
-        'LAYERS': 'ambiti_sistema_porti',
-        //'STYLES': null,
-    }
-    }),
-    minZoom: 0,
-    maxZoom: 28,
-    zIndex: 99,
-    opacity: 1.00
-});
-map.addLayer(ambitiSistemaPorti);
-// getElementInfo(""layerInfo"", confineComunale);*/
-
 var ambitiSistemaPorti = new ol.layer.Tile({
     source: new ol.source.TileWMS({
     url: wmsSource,
@@ -39,5 +23,80 @@ $('#ambitiSistemaPorti').on('change', function() {
         getElementInfo("layerInfo", ambitiSistemaPorti);
     } else {
         map.removeLayer(ambitiSistemaPorti);
+    }
+});
+
+var emergenzeArche = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+    url: wmsSource,
+    params: {
+        'LAYERS': 'aree_emergenza_archeol',
+    }
+    }),
+    minZoom: 0,
+    maxZoom: 28,
+    zIndex: 1,
+    opacity: 1.00
+});
+$('#emergenzeArche').on('change', function() {
+    let isChecked = $(this).is(':checked');
+    if (isChecked) {
+        map.addLayer(emergenzeArche);
+        getElementLegendImg('legendemergenzeArche', emergenzeArche, wmsSource, 'emergenzeArche');
+        layerOpacity(emergenzeArche, 'emergenzeArcheslider', 'OUTPUTemergenzeArche');
+        layerZIndex(emergenzeArche, 'emergenzeArchezIndex', 'OUTPUTemergenzeArche');
+        getElementInfo("layerInfo", emergenzeArche);
+    } else {
+        map.removeLayer(emergenzeArche);
+    }
+});
+
+var interessePaesaggistico = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+    url: wmsSource,
+    params: {
+        'LAYERS': 'aree_interesse_paesaggi_ptm',
+    }
+    }),
+    minZoom: 0,
+    maxZoom: 28,
+    zIndex: 1,
+    opacity: 1.00
+});
+$('#interessePaesaggistico').on('change', function() {
+    let isChecked = $(this).is(':checked');
+    if (isChecked) {
+        map.addLayer(interessePaesaggistico);
+        getElementLegendImg('legendinteressePaesaggistico', interessePaesaggistico, wmsSource, 'interessePaesaggistico');
+        layerOpacity(interessePaesaggistico, 'interessePaesaggisticoslider', 'OUTPUTinteressePaesaggistico');
+        layerZIndex(interessePaesaggistico, 'interessePaesaggisticozIndex', 'OUTPUTinteressePaesaggistico');
+        getElementInfo("layerInfo", interessePaesaggistico);
+    } else {
+        map.removeLayer(interessePaesaggistico);
+    }
+});
+
+var naturaliPregio = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+    url: wmsSource,
+    params: {
+        'LAYERS': 'aree_naturali_pregio_ptm',
+    }
+    }),
+    minZoom: 0,
+    maxZoom: 28,
+    zIndex: 1,
+    opacity: 1.00
+});
+$('#naturaliPregio').on('change', function() {
+    let isChecked = $(this).is(':checked');
+    if (isChecked) {
+        map.addLayer(naturaliPregio);
+        getElementLegendImg('legendnaturaliPregio', naturaliPregio, wmsSource, 'naturaliPregio');
+        layerOpacity(naturaliPregio, 'naturaliPregioslider', 'OUTPUTnaturaliPregio');
+        layerZIndex(naturaliPregio, 'naturaliPregiozIndex', 'OUTPUTnaturaliPregio');
+        getElementInfo("layerInfo", naturaliPregio);
+    } else {
+        map.removeLayer(naturaliPregio);
     }
 });
