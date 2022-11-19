86c58068298479d4d73b66e9e3e0ad6dc9aaa5f7
MaxDragonheart <massimilianomoraca@gmail.com>
2022-11-19 11:51:09 +0100

    dev project

--- assets/js/map.js
+++ assets/js/map.js
@@ -64,26 +64,48 @@ function getElementInfo(elementID, layerName){
           var json = JSON.parse(data).features[0]
           var layer_id = json.id;
           var layer_properties = json.properties;
-          console.log(typeof(layer_properties));
-          console.log(layer_properties);
+          // console.log(typeof(layer_properties));
+          // console.log(layer_properties);
 
           $(".modal-title").text(layer_id);
-          // $(".modal-body").text("prova prova");
           $("#clickOnPixelModal").modal('show');
 
-          var tableHeadContents = '<thead></thead>';
-          var tableHead = $(tableHeadContents).attr("id","thead");
-          $("#"+elementID).html('');
-          $("#"+elementID).append(tableHead);
-          var tableBodyContents = '<tbody></tbody>';
-          var tableBody = $(tableBodyContents).attr("id","tbody"+elementID);
-          $("#"+elementID).append(tableBody);
+          var modalBodyId = document.getElementById(elementID);
+          // console.log(modalBodyId)
+
+          // Make table and put inside all data
+          var tableDefinition = document.createElement("table");
+          tableDefinition.classList.add("table");
+          tableDefinition.classList.add("table-popup");
+          var tableBody = document.createElement("tbody");
+          tableDefinition.appendChild(tableBody);
+
           for (const [key, value] of Object.entries(layer_properties)) {
+            var tableRow = document.createElement("tr");
+            var rowHead = document.createElement("th");
+            rowHead.classList.add("td-head")
+            var rowCell = document.createElement("td")
+            rowCell.classList.add("td-body")
             // console.log(key, value);
-            tableRow = '<tr><td class="td-head">' + key + '</td><td class="td-body">' + value + '</td></tr>';
-            $("#tbody"+elementID).append(tableRow);
+
+            var headContents = document.createTextNode(key + ": ");
+            rowHead.appendChild(headContents);
+
+            if (value == null) {
+              var cellContents = document.createTextNode("NoData");
+            } else {
+              var cellContents = document.createTextNode(value);
+            }
+            rowCell.appendChild(cellContents);
+
+            tableRow.appendChild(rowHead);
+            tableRow.appendChild(rowCell);
+
+            tableBody.appendChild(tableRow);
+
           }
-          // console.log(tableHeadContents);
+          // console.log(tableDefinition);
+          modalBodyId.appendChild(tableDefinition);
 
         }
       } catch (e) {
