d0bbca554751dc7d7c85bae7d6a3bfb489ee5228
MaxDragonheart <massimilianomoraca@gmail.com>
2022-11-19 16:49:31 +0100

    dev project

--- assets/js/layers_sit_cmn_cartografia_storica.js
+++ assets/js/layers_sit_cmn_cartografia_storica.js
@@ -1,27 +1,44 @@
 // List of layers from Città Metropolitana di Napoli
+proj4.defs('EPSG:32633', '+proj=utm +zone=33 +datum=WGS84 +units=m +no_defs +type=crs');
+ol.proj.proj4.register(proj4);
+
 var wmtsSource = 'http://sit.cittametropolitana.na.it/geoserver/gwc/service/wmts'
 
-var raster2021 = new ol.layer.Tile({
-    source: new ol.source.WMTS({
-        url: wmtsSource,
-        layer: 'raster:2021_Worldview3_RGB',
-        format: 'image/jpg',
-        style: 'default',
-        wrapX: true,
-    }),
-    // minZoom: 0,
-    // maxZoom: 28,
-    // zIndex: 1,
-    // opacity: 1.00
+var raster2021 = new ol.layer.Tile();
+//map.addLayer(raster2021);
+
+// fetch(wmtsSource + '?SERVICE=WMTS&request=GetCapabilities')
+//   .then(function (response) {
+//     return response.text();
+//   })
+//   .then(function (text) {
+//     var result = new ol.format.WMTSCapabilities().read(text);
+//     var options = ol.source.WMTS.optionsFromCapabilities(result, {
+//       layer: 'raster:2021_Worldview3_RGB',
+//     });
+//     raster2021.setSource(new ol.source.WMTS(options));
+//   });
+
+$('#raster2021').on('change', function() {
+    let isChecked = $(this).is(':checked');
+    if (isChecked) {
+
+        fetch(wmtsSource + '?SERVICE=WMTS&request=GetCapabilities')
+          .then(function (response) {
+            return response.text();
+          })
+          .then(function (text) {
+            var result = new ol.format.WMTSCapabilities().read(text);
+            var options = ol.source.WMTS.optionsFromCapabilities(result, {
+              layer: 'raster:2021_Worldview3_RGB',
+            });
+            raster2021.setSource(new ol.source.WMTS(options));
+          });
+
+        map.addLayer(raster2021);
+        layerOpacity(raster2021, 'raster2021slider', 'OUTPUTraster2021');
+        layerZIndex(raster2021, 'raster2021zIndex', 'OUTPUTraster2021');
+    } else {
+        map.removeLayer(raster2021);
+    }
 });
-map.addLayer(raster2021);
-// $('#raster2021').on('change', function() {
-//     let isChecked = $(this).is(':checked');
-//     if (isChecked) {
-//         map.addLayer(raster2021);
-//         layerOpacity(raster2021, 'raster2021slider', 'OUTPUTraster2021');
-//         layerZIndex(raster2021, 'raster2021zIndex', 'OUTPUTraster2021');
-//     } else {
-//         map.removeLayer(raster2021);
-//     }
-// });
--- assets/js/test.js
+++ assets/js/test.js
@@ -19,17 +19,25 @@ var osm = new ol.layer.Tile({
 });
 map.addLayer(osm);
 
-// var wmtsSource = 'http://sit.cittametropolitana.na.it/geoserver/gwc/service/wmts'
-//
-// var raster2021 = new ol.layer.Tile({
-//     source: new ol.source.WMTS({
-//         url: wmtsSource,
-//         layer: 'raster:2021_Worldview3_RGB',
-//     }),
-// });
-// map.addLayer(raster2021);
-//
-// console.log(map.getView().getProjection())
+proj4.defs('EPSG:32633', '+proj=utm +zone=33 +datum=WGS84 +units=m +no_defs +type=crs');
+ol.proj.proj4.register(proj4);
+
+var wmtsSource = 'http://sit.cittametropolitana.na.it/geoserver/gwc/service/wmts'
+
+var raster2021 = new ol.layer.Tile();
+map.addLayer(raster2021);
+
+fetch(wmtsSource + '?SERVICE=WMTS&request=GetCapabilities')
+  .then(function (response) {
+    return response.text();
+  })
+  .then(function (text) {
+    var result = new ol.format.WMTSCapabilities().read(text);
+    var options = ol.source.WMTS.optionsFromCapabilities(result, {
+      layer: 'raster:2021_Worldview3_RGB',
+    });
+    raster2021.setSource(new ol.source.WMTS(options));
+  });
 
 // function getElementInfo(elementID, layerName){
 //   map.on('singleclick', function (event) {
